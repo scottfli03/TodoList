e9742643bce56bfa423d074fffc6a28bce7bfea5
Scott Flischel <scott.flischel@gmail.com>
2017-06-02 16:04:01 -0400

    Displaying list in table form with description.

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 (function() { var app = angular.module("myApp",[ ]);
-	var sampleLists   =	[{ 
+	var sampleLists   =	[{  
+							id: 1,
 							title: "Hygene Products",
-							listItems: [{title: "Soap", description: "Kind that smells good."},
-									{title: "Shaving cream", description: "Sensitive Skin"}],
+							listItems: [{id: 1, title: "Soap", description: "Kind that smells good.", completed: false},
+										{id: 2, title: "Comb", description: "A giant one!", completed: false},
+										{id: 3, title: "Shaving cream", description: "Sensitive Skin", completed: false}],
 						},
 						{
+							id: 2,
 							title: "School Supplies",
-							listItems: [{title: "Computer", description: "Laptop"},
-										{title: "Notebooks", description: "Just in case you take notes by hand."}]
+							listItems: [{id: 2, title: "Computer", description: "Laptop", completed: false},
+										{id: 1, title: "Notebooks", description: "Just in case you take notes by hand.", completed: false}]
 						}];	
 	    
 
-	app.controller("ListController", function() {
-		this.lists = sampleLists;
-	});
-
 	app.controller("ListSetController", function() {
-
+		this.lists = sampleLists;	
 	});
 })();
