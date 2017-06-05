8e7b016608d41c8852ff5e2b83b5bc8cdaa22f27
Scott Flischel <scott.flischel@gmail.com>
2017-06-05 15:22:33 -0400

    refactored code, removed list objects from app.js, added them to lists.json and imported json file.

--- app.js
+++ app.js
@@ -1,38 +1,23 @@
-(function() { var app = angular.module("myApp",[ ]);
-	var sampleLists   =	[{  
-							id: 1,
-							title: "Hygene Products",
-							listItems: [{id: 1, title: "Soap", description: "Kind that smells good.", completed: false},
-										{id: 2, title: "Comb", description: "A giant one!", completed: false},
-										{id: 3, title: "Shaving cream", description: "Sensitive Skin", completed: false}],
-						},
-						{
-							id: 2,
-							title: "School Supplies",
-							listItems: [{id: 2, title: "Computer", description: "Laptop", completed: false},
-										{id: 1, title: "Notebooks", description: "Just in case you take notes by hand.", completed: false}]
-						}];	
-	    
+(function() { var app = angular.module("myApp",[ ]);	    
 
-	app.controller("ListSetController", function() {
-		this.lists = sampleLists;
+	app.controller("ListSetController", ['$http', function($http) {
+		var self = this;
+		var newItem = {};
+		self.newItem = newItem;
+		try {
+			$http.get('lists.json').then(function(res){
+          		self.lists = res.data;                
+        		}
+        	);
+		} 
+		catch(err) {
+			console.log(err);
+		}
 
-		this.addList = function(list) {
-			this.lists.push(list);
-		};
-
-		this.addListItem = function(listID, item) {
-			var maxID = 1,
-				i;
-			for (i = 0; i < this.lists[listID].length; i++) {
-				if (this.lists[listID].listItems[i].id > maxID) {
-					maxID = this.lists[listID].listItems[i].id;
-				}
-			}
-			item.id = maxID;
-			item.completed = false;
-			this.lists[listID].listItems.push(item);
-
-		}	
-	});
+		this.addListItem = function(list) {
+			console.log(list);
+			self.newItem.completed = false;
+			list.listItems.push(self.newItem);
+		};	
+	}]);
 })();
