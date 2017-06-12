fcbd8babbd0e77bd42317f54fac8cf4f972f5d98
Scott Flischel <scott.flischel@gmail.com>
2017-06-12 10:46:24 -0400

    Add list and add list item working across components. Used https://stackoverflow.com/questions/29762926/passing-function-arguments-across-multiple-isolated-scopes-with-angular-directiv as a reference.

--- lists.js
+++ lists.js
@@ -10,23 +10,17 @@ angular.module("myApp").component('listApp', {
   	self.addList = function(title) {
 			console.log("Attempting to add a new list to:\n" + self.lists);
   		self.lists.push({title: title,
-  			itemsList: []
+  			listItems: [],
   		});
   		console.log("After adding your list:\n" + self.lists);
   	};
-  	self.addListItem = function(obj) {
+  	self.addListItem = function(list, newItem) {
 			console.log("addListItem called");
-			console.log(this);
-  		console.log(obj);
-  		console.log(description);
-			list.listItems.push({title: title, description: description, completed: false});
+			var newItemCopy = angular.copy(newItem);
+			console.log(self);
+			list.listItems.push(newItemCopy);
 			console.log(self.lists);
 		};
-		// self.updateLists = function(event) {
-		// 	console.log("updateLists called.");
-		// 	self.lists = event.lists;
-		// 	console.log(self.lists);
-		// };
   },
 	templateUrl: 'templates/listApp.html'
 });
@@ -43,28 +37,6 @@ angular.module("myApp").component('listTables', {
 
 		console.log("Entered ListTables Controller");
 		console.log(self);
-		// self.updateList = function(event) {
-		// 	console.log("updateList called:\nself.list:\n"+self.list+"\nevent.list:\n" + event.list);
-		// 	self.list = event.list;
-		// };
-		// self.$onChanges = function(changes) {
-		// 	console.log("Entered listTables $onChange method.");
-		// 	if (changes.list && self.list !== undefined) {
-		// 		self.list = angular.copy(changes.list.currentValue);
-		// 		console.log(self.list);
-		// 	}
-		// 	else {
-		// 		console.log(self.list);
-		// 	}
-		// };
-		// self.saveList = function() {
-		// 	console.log("saveList called.")
-		// 	self.onListUpdate({
-		// 		$event: {
-		// 			list: self.list
-		// 		}
-		// 	});
-		// };
 	}
 });
 
@@ -76,25 +48,8 @@ angular.module("myApp").component('addListItemTable', {
 	templateUrl: 'templates/addListItemTable.html',
 	controller: function() {
 		var self = this;
-		self.newItem = {
-			title : '',
-			description : '',
-			completed : false
-		};
 		console.log("Entered addListItemTable Controller");
 		console.log(self);
-		// self.$onChanges = function(changes) {
-		// 	if(changes.list){
-		// 		self.list = angular.copy(changes.list.currentValue);
-		// 	}
-		// };
-		// self.saveList = function() {
-		// 	self.onListUpdate({
-		// 		$event: {
-		// 			list: self.list
-		// 		}
-		// 	})
-		// };
 	}
 });
 
@@ -105,23 +60,8 @@ angular.module("myApp").component('newListForm', {
 	templateUrl: 'templates/newListForm.html',
 	controller: function() {
 		self = this;
-		self.addList('testList');
 		console.log("Entered newListForm Controller");
 		console.log(self);
-		// self.$onChanges = function(changes) {
-		// 	console.log("newListForm $onChanges method called.");
-		// 	if (changes.lists && self.lists !== undefined) {
-		// 		self.lists = angular.copy(self.lists.currentValue);
-		// 	}
-		// };
-		// self.saveLists = function() {
-		// 	console.log("saveLists called.");
-		// 	self.onListsUpdate({
-		// 		$event: {
-		// 			lists: self.lists
-		// 		}
-		// 	})
-		// };
 	}
 });
 
