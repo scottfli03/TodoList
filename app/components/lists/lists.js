e8d8cc9434ab8e5d6a75218284fcef61c10c84d9
Scott Flischel <scott.flischel@gmail.com>
2017-06-21 14:56:01 -0400

    Removed localStorage for testing simplification. All functions now have some testing.

--- app/components/lists/lists.js
+++ app/components/lists/lists.js
@@ -2,60 +2,88 @@ angular.module("myApp",['ngStorage']).component('listApp', {
 	controller: function($scope, $http, $localStorage) {
 		var self = this;
 		// delete $localStorage.lists;
-		console.log("Entered List App Controller");
-		if (typeof $localStorage.lists === "undefined") {
-			self.$onInit = $http.get('assets/JSON/lists.json').then(function(res){
-				console.log("Attempting to access data.");
-	  		self.lists = res.data;
-	  		self.saveListData();           
-			});
-		} else {
-			self.lists = localStorage.lists;
-		}
-		function loadListData() {
-			self.lists = $localStorage.lists;
-		}
-		self.saveListData = function() {
-			$localStorage.lists = self.lists;
-		}
+		self.lists = [  
+      {"title":"Hygiene Products", 
+      "isNew":false,
+      "listItems": [
+        {"title": "Soap", 
+        "description": "Kind that smells good.",
+        "isSelected": true, 
+        "completed": false},
+        {"title": "Comb", 
+        "description": "A giant one!", 
+        "isSelected": false,
+        "completed": false},
+        {"title": "Shaving cream", 
+        "description": "Sensitive Skin",
+        "isSelected": false, 
+        "completed": false}]},
+      {"title": "School Supplies",
+      "isNew": false,
+      "listItems": [
+        {"title": "Computer", 
+        "description": "Laptop",
+        "isSelected": false, 
+        "completed": false},
+        {"title": "Notebooks", 
+        "description": "Just in case you take notes by hand.",
+        "isSelected": true, 
+        "completed": true}]}
+    ];
+		// self.getLists = function() {
+		// 	if (typeof $localStorage.lists === "undefined") {
+		// 		$http.get('assets/JSON/lists.json').then(function(res){
+		// 			console.log("Attempting to access data.");
+		//   		self.lists = res.data;
+		//   		self.saveListData();           
+		// 		});
+		// 	} else {
+		// 		self.lists = localStorage.lists;
+		// 	}
+		// };
+
+		// self.loadListData = function() {
+		// 	self.lists = $localStorage.lists;
+		// }
+
+		// self.saveListData = function() {
+		// 	$localStorage.lists = self.lists;
+		// }
+
   	self.addList = function(title) {
-			console.log("Attempting to add a new list to:\n" + self.lists);
   		self.lists.push({title: title,
   			isNew: true,
   			listItems: [],
   		});
-  		console.log("After adding your list:\n" + self.lists);
   	};
+
   	self.removeList = function(list) {
   		var index = self.lists.indexOf(list);
   		self.lists.splice(index, 1);
   	};
+
   	self.addListItem = function(list, newItem) {
-			console.log("addListItem called");
 			newItem["isSelected"] = false;
 			newItem["completed"] = false;
 			list.isNew = false;
 			var newItemCopy = angular.copy(newItem);
 			list.listItems.push(newItemCopy);
 		};
+
 		self.updateListItem = function(list, listItem) {
-			console.log("updateListItem called");
 			var listIndex = self.lists.indexOf(list);
 			var itemIndex = list.listItems.indexOf(listItem);
 			var listItemCopy = angular.copy(listItem);
 			self.lists[listIndex].listItems[itemIndex] = listItem;
 		};
+
 		self.removeListItem = function(list, listItem) {
-			console.log("removeListItem called");
-			console.log(list);
-			console.log(listItem);
 			var listIndex = self.lists.indexOf(list);
 			var itemIndex = list.listItems.indexOf(listItem);
-			console.log(itemIndex);
 			self.lists[listIndex].listItems.splice(itemIndex, 1);
 		};
+
 		self.toggleCompleted = function(listType, list) {
-			console.log("setComplete called");
 			var liLength = list.listItems.length;
 			var index = self.lists.indexOf(list);
 			var liIsSelected;
@@ -67,10 +95,11 @@ angular.module("myApp",['ngStorage']).component('listApp', {
 					list.listItems[i].completed = false;
 				}
 			}
-			self.setUnSelected();
+			setUnSelected();
 			self.lists[index] = angular.copy(list);
 		};
-		self.setUnSelected = function() {
+
+		var setUnSelected = function() {
 			var listsLength = self.lists.length;
 			var i, a, theList, liLength;
 			for (i = 0; i < listsLength; i++) {
@@ -80,11 +109,11 @@ angular.module("myApp",['ngStorage']).component('listApp', {
 					theList.listItems[a]["isSelected"] = false;
 				}
 			}
-		};
-		if (typeof $localStorage.lists !== "undefined") {
-			loadListData();
-		}
-		console.log(self);
+		};	
+		// self.getLists();
+		// if (typeof $localStorage.lists !== "undefined") {
+		// 	loadListData();
+		// }
   },
 	templateUrl: 'app/components/lists/listApp.html',
 	bindings: {
